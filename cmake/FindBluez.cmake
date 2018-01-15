3fd9feacd121b766daa71bc6894e0d3abe836afe
Sergey Savelyev <sergeysav.nn@gmail.com>
2018-01-15 16:49:03 -0500

    Updates Bluez cmake file

--- cmake/FindBluez.cmake
+++ cmake/FindBluez.cmake
@@ -1,26 +1,18 @@
-# - try to find Bluez
+# - Try to find BlueZ
+# Find BlueZ headers, libraries and the answer to all questions.
 #
-# Cache Variables: (probably not for direct use in your scripts)
-#  BLUEZ_INCLUDE_DIR
-#  BLUEZ_LIBRARY
+#  BLUEZ_FOUND               True if BlueZ libraries got found
+#  BLUEZ_INCLUDE_DIR         Location of BlueZ headers 
+#  BLUEZ_LIBRARIES           List of libaries to use BlueZ
 #
-# Non-cache variables you might use in your CMakeLists.txt:
-#  BLUEZ_FOUND
-#  BLUEZ_INCLUDE_DIRS
-#  BLUEZ_LIBRARIES
+# Copyright (c) 2007 Daniel Gollub <gollub@b1-systems.de>
+# Copyright (c) 2007-2009 Bjoern Ricks  <bjoern.ricks@gmail.com>
 #
-# Requires these CMake modules:
-#  FindPackageHandleStandardArgs (known included with CMake >=2.6.2)
+#  Redistribution and use is allowed according to the terms of the New
+#  BSD license.
+#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
-# Original Author:
-# 2009-2010 Ryan Pavlik <rpavlik@iastate.edu> <abiryan@ryand.net>
-# http://academic.cleardefinition.com
-# Iowa State University HCI Graduate Program/VRAC
-#
-# Copyright Iowa State University 2009-2010.
-# Distributed under the Boost Software License, Version 1.0.
-# (See accompanying file LICENSE_1_0.txt or copy at
-# http://www.boost.org/LICENSE_1_0.txt)
+
 
 if(WIN32 OR APPLE OR NOT UNIX)
     if(NOT Bluez_FIND_QUIETLY)
@@ -29,51 +21,46 @@ if(WIN32 OR APPLE OR NOT UNIX)
         return()
     endif()
 else()
-    set(BLUEZ_ROOT_DIR
-            "${BLUEZ_ROOT_DIR}"
-            CACHE
-            PATH
-            "Directory to search")
+	INCLUDE( FindPkgConfig )
 
-    if(CMAKE_SIZEOF_VOID_P MATCHES "8")
-        set(_LIBSUFFIXES lib64 lib)
-    else()
-        set(_LIBSUFFIXES lib)
-    endif()
+	# Take care about bluez.pc settings
+	IF ( BlueZ_FIND_REQUIRED )
+		SET( _pkgconfig_REQUIRED "REQUIRED" )
+	ELSE( BlueZ_FIND_REQUIRED )
+		SET( _pkgconfig_REQUIRED "" )	
+	ENDIF ( BlueZ_FIND_REQUIRED )
 
-    find_library(BLUEZ_LIBRARY
-            NAMES
-            bluez/bluetooth
-            HINTS
-            "${BLUEZ_ROOT_DIR}"
-            PATH_SUFFIXES
-            "${_LIBSUFFIXES}")
+	IF ( BLUEZ_MIN_VERSION )
+		PKG_SEARCH_MODULE( BLUEZ ${_pkgconfig_REQUIRED} bluez>=${BLUEZ_MIN_VERSION} )
+	ELSE ( BLUEZ_MIN_VERSION )
+		PKG_SEARCH_MODULE( BLUEZ ${_pkgconfig_REQUIRED} bluez )
+	ENDIF ( BLUEZ_MIN_VERSION )
 
-    # Might want to look close to the library first for the includes.
-    get_filename_component(_libdir "${BLUEZ_LIBRARY}" PATH)
+	# Look for BlueZ include dir and libraries
+	IF( NOT BLUEZ_FOUND AND NOT PKG_CONFIG_FOUND )
 
-    find_path(BLUEZ_INCLUDE_DIR
-            NAMES
-            bluez/bluetooth/bluetooth.h
-            HINTS
-            "${_libdir}/.."
-            PATHS
-            "${BLUEZ_ROOT_DIR}"
-            PATH_SUFFIXES
-            include/)
+		FIND_PATH( BLUEZ_INCLUDE_DIR bluetooth/bluetooth.h )
+		FIND_LIBRARY( BLUEZ_LIBRARIES bluetooth )
+
+		# Report results
+		IF ( BLUEZ_LIBRARIES AND BLUEZ_INCLUDE_DIR )	
+			SET( BLUEZ_FOUND 1 )
+			IF ( NOT BlueZ_FIND_QUIETLY )
+				MESSAGE( STATUS "Found BlueZ: ${BLUEZ_LIBRARIES}" )
+			ENDIF ( NOT BlueZ_FIND_QUIETLY )
+		ELSE ( BLUEZ_LIBRARIES AND BLUEZ_INCLUDE_DIR )	
+			IF ( BlueZ_FIND_REQUIRED )
+				MESSAGE( SEND_ERROR "Could NOT find BLUEZ" )
+			ELSE ( BlueZ_FIND_REQUIRED )
+				IF ( NOT BlueZ_FIND_QUIETLY )
+					MESSAGE( STATUS "Could NOT find BLUEZ" )	
+				ENDIF ( NOT BlueZ_FIND_QUIETLY )
+			ENDIF ( BlueZ_FIND_REQUIRED )
+		ENDIF ( BLUEZ_LIBRARIES AND BLUEZ_INCLUDE_DIR )
+
+	ENDIF( NOT BLUEZ_FOUND AND NOT PKG_CONFIG_FOUND  )
+
+	# Hide advanced variables from CMake GUIs
+	MARK_AS_ADVANCED( BLUEZ_LIBRARIES BLUEZ_INCLUDE_DIR )
+
 endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Bluez
-        DEFAULT_MSG
-        BLUEZ_LIBRARY
-        BLUEZ_INCLUDE_DIR)
-
-if(BLUEZ_FOUND)
-    set(BLUEZ_LIBRARIES "${BLUEZ_LIBRARY}")
-    set(BLUEZ_INCLUDE_DIRS "${BLUEZ_INCLUDE_DIR}")
-    mark_as_advanced(BLUEZ_ROOT_DIR)
-endif()
-
-mark_as_advanced(BLUEZ_INCLUDE_DIR
-        BLUEZ_LIBRARY)
